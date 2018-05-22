e8fcf3e6306cf60365919008b78cd4c7d9bd7cc4
Lugamo <bitlugamo@gmail.com>
2018-05-21 19:08:10 -0500

    setup for Heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
+
 const  app = express();
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials')
@@ -47,6 +49,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-   console.log('Example app listening on port 3000!')
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}`)
 });
