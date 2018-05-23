a36f418949f648fb432e2a0c45da2608135ea37b
Lugamo <bitlugamo@gmail.com>
2018-05-22 19:56:38 -0500

    adding

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const geocode = require('./modules/geocode/geocode.js')
+const forecast = require('./modules/forecast/forecast.js')
 
 const port = process.env.PORT || 3000;
 
@@ -14,6 +16,7 @@ hbs.registerHelper('getcurrentYear', () => {
 hbs.registerHelper('getCapitalize', (text) => {
   return text.toUpperCase();
 });
+
 
 app.use((req, res, next) => {
   var now = new Date().toString();
@@ -54,9 +57,35 @@ app.get('/bad', (req, res) => {
 app.get('/projects', (req, res) => {
   res.render('projects.hbs', {
     pageTitle: 'Projects',
-    NumberOfProjects: 7
+    NumberOfProjects: 2
   })
 });
+app.get('/weather', (req, res) => {
+  res.render('weather.hbs', {
+    pageTitle: 'Projects',
+    NumberOfProjects: 2
+  })
+});
+
+// app.get('/weather/:?city', function(req, res) {
+//   geocode.geocodeAddress(req.params.city, (error, result) => {
+//      if (error) {
+//          console.log(error)
+//      }else {
+//          console.log(result.address)
+//          forecast.getForecast(result.latitude,result.longitude, (error, result) =>{
+//              if (error) {
+//                  console.log(error)
+//              }else {
+//                res.send('temperature ' + result.temperature);
+//                //console.log(`The currently temperature is ${result.temperature} but it feels like ${result.apparent_temperature}`)
+//              }
+//          })
+//      }
+//   });
+//
+// });
+
 
 app.listen(port, () => {
    console.log(`Example app listening on port ${port}`)
